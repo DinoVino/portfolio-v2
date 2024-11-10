566db443e4eb9bbc1a47eb8914f3397da080e602
ursavin <donovankhoun@gmail.com>
2024-11-10 23:48:52 +0100

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<style lang="css">
+    nav{
+        text-align: center;
+    }
+    a{
+        text-decoration:none;
+    }
+</style>
+
+<script>
+  let {children} = $props();
+</script>
+
+<nav>
+    <a href="/">Home </a>
+    <a href="/about">About me</a>
+    <a href="/projects"> Projects </a>
+</nav>
+<div class="middle">
+    {@render children()}
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,19 @@
+<h1>
+    Who am I?
+</h1>
+
+<p>
+    Hi! I am Donovan Khoun
+</p>
+
+<h2>
+    What do you professionally do?
+</h2>
+
+<p>
+    I am currently a Functional Consultant at a company whilst moonlighting as a data engineer.
+    Studying software development at the same time proves to be beneficial for them, as I have
+    an understanding of how things "should" work in the optimal state.
+    The course is given at Fontys and is called "HBO-ICT".
+    As of writing this page, I am in the 6th semester.
+</p>
