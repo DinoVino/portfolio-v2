df7a7dead1b039169a1fd26c55307f7bd2443b1d
Ursavin <donovankhoun@gmail.com>
2024-11-12 13:01:05 +0000

    writing the nixos project + adding styling for the nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,16 @@
 <nav class="suse">
     <a href="/">Home </a>
     <a href="/about">About me</a>
-    <a href="/projects"> Projects </a>
+    <div class="dropdown">
+        <a href="/projects"> Projects </a>
+        <div class="content">
+            <a href="/projects/nixos">test</a>
+            <a href="/projects/innerev">twst</a>
+        </div>
+
+    </div>
+    
+    
 </nav>
 <div class="middle">
     {@render children()}
---
+++ src/routes/projects/nixos/+page.svelte
@@ -0,0 +1,22 @@
+<h2>
+    Creating a reproducible Operating System with NixOS
+</h2>
+<h3>
+    Why did I start this project?
+</h3>
+<p>
+    Since I have been using Linux for about 4 years, and heard about the elusive distro NixOS,
+    I wanted to give it a shot. There are some really neat features that resonate with me.
+    In the past, I had been distrohopping a lot: jumping from one linux distro to the other. 
+    None of them really scratched my itch. Aside from the distrohopping, I also switch hardware a lot.
+    Constantly having to setup my workspace takes a lot of time and not every setup worked out of the box
+    due to differences in specs. 
+    One of the other issues I ran into was that my operating system got poluted really quickly. 
+    I work on so many different projects with varying programming languages and versions. 
+    Maintaining this was really hard even though there were solutions because they were all
+    related to the programming language. I had to manage the tools to manage the workenvironment 
+    in order to work on the project. This is reaching kingdom hearts levels of convolution.
+    Actually working on the projects was something I could only dream of after reliving the nightmare of managing the
+    different environments.
+
+</p>
